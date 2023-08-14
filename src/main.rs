7dfeeb6b36eb0d95438bdc4dc3fd09b59f1003f7
Neighborkid01 <calebproland@gmail.com>
2023-08-14 12:42:11 -0400

    Computing expected value to compare with experimental value

--- src/main.rs
+++ src/main.rs
@@ -24,9 +24,10 @@ fn main() {
             rolls_needed = 0;
         }
 
+        let one_through_n = (1..=num_die_faces).collect::<Vec<usize>>();
+        let one_over_k = one_through_n.iter().fold(0.0, |acc, &k| acc + (1.0/k as f64));
         let die_roll_avg = rolls_needed_hash.iter().fold(0, |acc, (k, v)| acc + k * v) as f64 / sample_size as f64;
-        println!("{}: {}", num_die_faces, die_roll_avg);
+        println!("{} expected: {}, got: {}", num_die_faces,  num_die_faces as f64 * one_over_k, die_roll_avg);
         die_roll_avgs.insert(num_die_faces, die_roll_avg);
     }
-
 }
